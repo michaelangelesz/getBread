c017cb079d5e0a492a70bbd1a99c44c913d01d3d
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-07-08 11:15:54 -0400

    code along, adding routes

--- controllers/bread.js
+++ controllers/bread.js
@@ -1,8 +1,14 @@
 const router = require('express').Router()
+const Bread = require('../models/bread')
 
 // Path: controllers/bread.js
 router.get('/', (req, res) => {
-    res.send('BREAD')
+    res.send(Bread)
+})
+
+router.get('/:index', (req, res) => {
+    const { index } = req.params
+    res.send(Bread[index])
 })
 
 module.exports = router
